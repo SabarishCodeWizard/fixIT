b0b8a2b4f4477e709645e599dcf06464323260cb
SabarishCodeWizard <ravik60656@gmail.com>
2024-09-19 21:20:19 +0530

    we can provide that as well, along with other services tailored to your specific requirements added

--- src/App.js
+++ src/App.js
@@ -289,7 +289,7 @@ function App() {
                       />
                       <figcaption className="carousel-caption">
 
-                        <p className="testimonial-name">- Prabha Karan K</p>
+                        <p className="testimonial-name">- PrabhaKaran K</p>
                       </figcaption>
                     </figure>
                   </div>
